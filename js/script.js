04faebee5bb3ab2ca6097d22f9f7c63760122ea4
sophie-nguyen-0 <113055711+sophie-nguyen-0@users.noreply.github.com>
2022-10-26 18:17:19 +0000

    js

--- js/script.js
+++ js/script.js
@@ -17,5 +17,11 @@ if (navigator.serviceWorker) {
  * this function
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  const base1 = parseInt(document.getElementById("base-1").value)
+  const base2 = parseInt(document.getElementById("base-2").value)
+  const height = parseInt(document.getElementById("height-number").value)
+
+  const area = (( base1 + base2 ) / 2) * height
+
+  document.getElementById("area").innerHTML = "<h5>The area is " + area + "</h5>"
 }
